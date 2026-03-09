809f82759824865422313df29b36608322960957
Viktor1009 <viktorosterberg10@gmail.com>
2026-03-09 12:22:27 +0100

    added code for a footer

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -3,7 +3,34 @@
 ?>
 
 <footer>
-    Some footer text
-</footer>
+        <div class="about">
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae velit sit amet purus auctor hendrerit vel vitae risus. Fusce tempus elit velit. Suspendisse potenti. Morbi est mauris, scelerisque id turpis vel, pretium consequat metus. Fusce molestie libero vel neque faucibus tempus. Etiam dignissim placerat dapibus.</p>
+        </div>
+        <div class="socials">
+            <h3>Socials:</h3>
+            <div>
+                <div class="socials-container">
+                    <img src="../assets/image/Paomedia-Small-N-Flat-Social-facebook.512.png" alt="facebook">
+                </div>
+                <div class="socials-container">
+                    <img src="../assets/image/Bokehlicia-Pacifica-Twitter.256.png" alt="twitter">
+                </div>
+                <div class="socials-container">
+                    <img src="../assets/image/Pinterest-logo.png" alt="pintrest">
+                </div>
+                <div class="socials-container">
+                    <img src="../assets/image/pngtree-instagram-icon-instagram-logo-png-image_3584853.png" alt="instagram">
+                </div>
+                <div class="socials-container">
+                    <img src="../assets/image/4834215.png" alt="tik-tok">
+                </div>
+            </div>
+        </div>
+        <div class="support">
+            <h3>Contact us</h3>
+            <a href="#"><h5>E-mail</h5></a>
+        </div>
+    </footer>
 </body>
 </html>
