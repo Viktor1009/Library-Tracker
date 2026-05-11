9c02f8baf08224cecd946ebae4890f171acd7ca6
Viktor1009 <viktorosterberg10@gmail.com>
2026-05-11 13:11:06 +0200

    activity only updates when page number changes

--- public/dashboard/update.php
+++ public/dashboard/update.php
@@ -14,11 +14,14 @@
             $stmt->execute();
             $stmt->close();
 
-            $sql = "INSERT INTO Activity (user_id, book_id) VALUES (?, ?)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ii", $user_id, $_POST["update_id"]);
-            $stmt->execute();
-            $stmt->close();
+            if($_POST["old_update_page"] != $_POST["update_page"]){ // för att endast skicka updates till activity när sidonummer ändras
+
+                $sql = "INSERT INTO Activity (user_id, book_id) VALUES (?, ?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("ii", $user_id, $_POST["update_id"]);
+                $stmt->execute();
+                $stmt->close();
+            }
 
             header("Location: index.php");
             exit();
@@ -46,16 +49,17 @@
                     readonly class="read-only"
                     ><?php echo $row["book_description"];?></textarea>
                 <input 
-                    type="text" name="update_status" 
-                    value="<?php echo $row["book_status"];?>">
-                <input 
                     type="number" name="update_page" min="0"
                     value="<?php echo $row["book_page"];?>">  
+                <input 
+                    type="number" name="old_update_page" min="0"
+                    value="<?php echo $row["book_page"];?>" hidden>  
                 <!-- Din personliga rating bör vara en slider-->
                 <textarea 
                     type="text"name="update_notes"
                     ><?php echo $row["book_notes"];?></textarea>
                 <input class="btn btn-primary" type="submit" name="posttype" value="Update">
+                
             </form>
         <?php
         }   
